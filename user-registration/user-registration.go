0a7d35c7b8969511173756cbe2be38c9d4898786
gabrielcedran <gabrielcedran@gmail.com>
2024-10-15 17:09:16 +0100

    Create dynamodb table on cdk stack and grant permission for the lambda function to access it

--- user-registration/user-registration.go
+++ user-registration/user-registration.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
@@ -20,18 +21,23 @@ func NewUserRegistrationStack(scope constructs.Construct, id string, props *User
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+	// myUserTable is the resource name used on cloud formation
+	userTable := awsdynamodb.NewTable(stack, jsii.String("UserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName:     jsii.String("user_table"),    // this is the actual table name
+		RemovalPolicy: awscdk.RemovalPolicy_DESTROY, // this allows cdk destroy to also remove the table
+	})
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("UserRegistrationQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
-
-	awslambda.NewFunction(stack, jsii.String("UserRegistrationFunction"), &awslambda.FunctionProps{
+	registrationFunction := awslambda.NewFunction(stack, jsii.String("UserRegistrationFunction"), &awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
 		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
 		Handler: jsii.String("main"),
 	})
+
+	userTable.GrantReadWriteData(registrationFunction) // even tho the lambda func and the table exist in the same stack, the function needs explicit permission to access the table
 
 	return stack
 }
