257d03732057dc1915f275b1ab85bbf580907e6f
gabrielcedran <gabrielcedran@gmail.com>
2024-10-15 16:54:49 +0100

    Api layer logic

--- user-registration/lambda/api/api.go
+++ user-registration/lambda/api/api.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"user-registration/database"
+	"user-registration/types"
 )
 
 type ApiHandler struct {
@@ -12,4 +14,28 @@ func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
 	return ApiHandler{
 		dbStore: dbStore,
 	}
+}
+
+func (api ApiHandler) RegisterUserHandler(event types.RegisterUser) error {
+	if event.Username == "" || event.Password == "" {
+		return fmt.Errorf("request has empty parameters")
+	}
+
+	userExists, err := api.dbStore.DoesUserExist(event.Username)
+
+	if err != nil {
+		return fmt.Errorf("there was an error checking if error exists %w", err)
+	}
+
+	if userExists {
+		return fmt.Errorf("a user with that username already exists")
+	}
+
+	err = api.dbStore.InsertUser(event)
+
+	if err != nil {
+		return fmt.Errorf("there was an error registering the user %w", err)
+	}
+
+	return nil
 }
