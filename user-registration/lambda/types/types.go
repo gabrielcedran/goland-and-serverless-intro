af8300ee7e6d73bca85dd7d4cc4b94cd18e9e2f0
gabrielcedran <gabrielcedran@gmail.com>
2024-10-15 22:54:48 +0100

    Password hashing

--- user-registration/lambda/types/types.go
+++ user-registration/lambda/types/types.go
@@ -1,6 +1,32 @@
 package types
 
+import "golang.org/x/crypto/bcrypt"
+
 type RegisterUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type User struct {
+	Username     string `json:"username"`
+	PasswordHash string `json:"password"`
+}
+
+func NewUser(registerUser RegisterUser) (*User, error) {
+	// []byte(registerUser.Password) -> transforms a string into a slice of bytes
+	// second parameter is the salt (the bigger the number, the long the processing to salt the string however more secure)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registerUser.Password), 10)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		Username:     registerUser.Username,
+		PasswordHash: string(hashedPassword),
+	}, nil
+}
+
+func ValidatePassword(hashedPassword, plainTextPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainTextPassword))
+	return err == nil
 }
